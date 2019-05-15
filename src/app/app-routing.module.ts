bd8cf0b97a5fdb19470d5dd26449e0020a1c151b
d-das <jdawsonweehunt@gmail.com>
2019-05-15 14:21:16 -0500

    basic theme established, routes layed down, generated all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { HowToInstallComponent } from './how-to-install/how-to-install.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "howitworks", component: HowItWorksComponent },
+  { path: "howtoinstall", component: HowToInstallComponent },
+  { path: "faq", component: FaqComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { HowToInstallComponent } from './how-to-install/how-to-install.component';
+import { ContactComponent } from './contact/contact.component';
+import { ApplicationCalculatorComponent } from './application-calculator/application-calculator.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FaqComponent,
+    HowItWorksComponent,
+    HowToInstallComponent,
+    ContactComponent,
+    ApplicationCalculatorComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
