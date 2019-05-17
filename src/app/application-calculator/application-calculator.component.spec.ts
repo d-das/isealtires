1a15761ea4e9eac7c0ec2ab0c6f46a540e7a47dd
d-das <jdawsonweehunt@gmail.com>
2019-05-17 15:53:05 -0500

    actually tracking components this time...

---
+++ src/app/application-calculator/application-calculator.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApplicationCalculatorComponent } from './application-calculator.component';
+
+describe('ApplicationCalculatorComponent', () => {
+  let component: ApplicationCalculatorComponent;
+  let fixture: ComponentFixture<ApplicationCalculatorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ApplicationCalculatorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ApplicationCalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/application-calculator/application-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-calculator',
+  templateUrl: './application-calculator.component.html',
+  styleUrls: ['./application-calculator.component.scss']
+})
+export class ApplicationCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
