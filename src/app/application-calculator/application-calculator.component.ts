28de67778bdad5284c9ecd274329af35e88d18e1
d-das <jdawsonweehunt@gmail.com>
2019-06-15 00:11:26 -0500

    first draft of sealant calculator

--- src/app/application-calculator/application-calculator.component.ts
+++ src/app/application-calculator/application-calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-application-calculator',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationCalculatorComponent implements OnInit {
 
-  constructor() { }
+  private factorA:number = 0.1780327;
+  private factorB:number = 3;
+  private factorC:number = 8;
+  @Input() public offRoad:boolean = true;
+  @Input() public width:number = 0;
+  @Input() public rim:number = 0;
+  public fluidOz: number = 0;
+  public handPumps: number =0;
+  changeLog: string[] = [];
+
+  constructor() {
+    console.log("greetings from app calc constructor");
+
+  }
 
   ngOnInit() {
   }
 
+  setOffRoad(){
+    this.offRoad = !this.offRoad;
+  }
+  calculate(){
+    let x = (this.width*this.rim)*this.factorA;
+    if(this.offRoad){
+      this.fluidOz = x*this.factorB;
+    }
+    else{
+      this.fluidOz =x;
+    }
+
+    this.handPumps = this.fluidOz/this.factorC;
+
+    console.log("fluidOz:", this.fluidOz);
+    console.log("handPumps:", this.handPumps);
+  }
 }
