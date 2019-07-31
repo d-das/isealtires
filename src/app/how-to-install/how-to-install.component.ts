1460cbb9c5fb7e3fe75cbfbfa68d20a14c01c27d
d-das <jdawsonweehunt@gmail.com>
2019-07-30 23:34:51 -0500

    updated theme, added instructions for installation

--- src/app/how-to-install/how-to-install.component.ts
+++ src/app/how-to-install/how-to-install.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-how-to-install',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowToInstallComponent implements OnInit {
 
+  public english:boolean = true;
+
+  setEnglish(){ this.english = true}
+  setSpanish(){ this.english = false}
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
